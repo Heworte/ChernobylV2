08b7643d335a5ab5256c152b831e4307b26a405c
Heworte <Ineee@LAPTOP-JEJEQ029>
2022-04-29 11:14:34 +0200

    Rozwinięcie żelaza

--- src/main/java/net/mcreator/chernobylv/block/RawCoalOreBlockBlock.java
+++ src/main/java/net/mcreator/chernobylv/block/RawCoalOreBlockBlock.java
@@ -23,12 +23,19 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
@@ -54,15 +61,36 @@ public class RawCoalOreBlockBlock extends ChernobylvModElements.ModElement {
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ChernobylItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
 	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 		public CustomBlock() {
 			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(2)
 					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
 			setRegistryName("raw_coal_ore_block");
 		}
 
 		@Override
 		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
 			return 15;
+		}
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		public BlockState rotate(BlockState state, Rotation rot) {
+			return state.with(FACING, rot.rotate(state.get(FACING)));
+		}
+
+		public BlockState mirror(BlockState state, Mirror mirrorIn) {
+			return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			;
+			return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
 		}
 	}
 	private static Feature<OreFeatureConfig> feature = null;
@@ -100,8 +128,8 @@ public class RawCoalOreBlockBlock extends ChernobylvModElements.ModElement {
 					return super.generate(world, generator, rand, pos, config);
 				}
 			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 30)).range(50)
-					.square().func_242731_b(30);
+			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 10)).range(50)
+					.square().func_242731_b(5);
 			event.getRegistry().register(feature.setRegistryName("raw_coal_ore_block"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("chernobylv:raw_coal_ore_block"), configuredFeature);
 		}
---
+++ src/main/java/net/mcreator/chernobylv/item/MediumRawIronIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.chernobylv.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.chernobylv.itemgroup.ChernobylItemGroup;
+import net.mcreator.chernobylv.ChernobylvModElements;
+
+@ChernobylvModElements.ModElement.Tag
+public class MediumRawIronIngotItem extends ChernobylvModElements.ModElement {
+	@ObjectHolder("chernobylv:medium_raw_iron_ingot")
+	public static final Item block = null;
+	public MediumRawIronIngotItem(ChernobylvModElements instance) {
+		super(instance, 18);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ChernobylItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("medium_raw_iron_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
